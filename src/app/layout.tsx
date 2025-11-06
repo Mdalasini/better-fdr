f1ccb13fafa2cd34db56c5ed49c77352f37af21c
Mdalasini <pathsofgrassymonkey@gmail.com>
2025-11-06 10:47:07 +0300

    feat: add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "MY FPL",
   description: "Fixture difficulty helper for Fantasy Premier League",
+  icons: "/images/logo.svg",
 };
 
 export default function RootLayout({
