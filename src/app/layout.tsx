751291b67ac8f587b9240132cf860f49aed5bf22
Mdalasini <pathsofgrassymonkey@gmail.com>
2025-11-23 15:11:23 +0300

    refactor: remove Navbar from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Navbar from "./components/Navbar";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,8 +16,6 @@ export default function DashboardLayout({
     <html lang="en">
       <body>
         <div className="min-h-screen bg-gray-50">
-          {/* Navigation */}
-          <Navbar />
           {/* Main Content */}
           <main className="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
             {children}
