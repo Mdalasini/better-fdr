561468ffa9640f5080996df67a93d8cc1c56ebe8
Mdalasini <pathsofgrassymonkey@gmail.com>
2025-11-23 15:27:42 +0300

    refactor: remove mongoose dependencies and code

--- src/lib/domain/TeamLogo.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const TeamLogoSchema = new Schema({
-  team_id: { type: String, required: true, unique: true },
-  logo_path: { type: String, required: true },
-});
-
-export default mongoose.models.TeamLogo ||
-  mongoose.model("team_logos", TeamLogoSchema);
